022018f3f7b13e540888db45f9519c97cff04b20
geore <jorgelopezchocarro@gmail.com>
2020-12-11 17:57:11 -0400

    Local Languages

---
+++ locale/en.php
@@ -0,0 +1,11 @@
+<?php
+
+class Translator {
+	public static $key = array(
+		"lang" => "English, Inglés",
+		"lang.slug" => "en_EN",
+	
+
+		"fin" => ""
+	);
+}
---
+++ locale/es.php
@@ -0,0 +1,11 @@
+<?php
+
+class Translator {
+	public static $key = array(
+		"lang" => "Spanish, Español",
+		"lang.slug" => "es_ES",
+		
+
+		"fin" => ""
+	);
+}
